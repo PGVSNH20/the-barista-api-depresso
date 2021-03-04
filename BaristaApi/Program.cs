b1b2b4691456141d6a0460508f4db842a7853e3b
Felix Englund <felix.englund@studerande.plushogskolan.se>
2021-03-04 12:02:45 +0100

    fixade IBeverage i program

--- BaristaApi/Program.cs
+++ BaristaApi/Program.cs
@@ -9,38 +9,52 @@ namespace BaristaApi
             try
 
             {
-                //CoffeeMachine espresso = new CoffeeMachine()
-                //.AddWater(50, 90)
-                //.AddBeans(new Bean(10, Bean.BeanTypes.Robusta))
-                //.GrindBeans();
-                //CoffeeMachine cappuccino = new CoffeeMachine()
-                //                            .AddWater(50, 85)
-                //                            .AddBeans(new Bean(5, Bean.BeanTypes.Arabica))
-                //                            .GrindBeans()
-                //                            .AddMilk(25)
-                //                            .AddFoamMilk(10);
+                IBeverage Espresso = new CoffeeMachine()
+                .AddWater(50, 90)
+                .AddBeans(new Bean(10, Bean.BeanTypes.Robusta))
+                .GrindBeans()
+                .ToBeverage();
 
-                //CoffeeMachine americano = new CoffeeMachine()
-                //    .AddWater(50, 85)
-                //    .AddBeans(new Bean(5, Bean.BeanTypes.Arabica))
-                //    .GrindBeans();
+                IBeverage Latte = new CoffeeMachine()
+                   .AddWater(50, 85)
+                   .AddBeans(new Bean(5, Bean.BeanTypes.Arabica))
+                   .AddMilk(35)
+                   .GrindBeans()
+                   .ToBeverage();
+
+                IBeverage Cappuccino = new CoffeeMachine()
+                                            .AddWater(50, 85)
+                                            .AddBeans(new Bean(5, Bean.BeanTypes.Arabica))
+                                            .GrindBeans()
+                                            .AddMilk(25)
+                                            .AddFoamMilk(10)
+                                            .ToBeverage();
+
+                IBeverage Americano = new CoffeeMachine()
+                    .AddWater(50, 85)
+                    .AddBeans(new Bean(5, Bean.BeanTypes.Arabica))
+                    .GrindBeans()
+                    .ToBeverage();
 
 
 
-                //                        CoffeeMachine macchiato = new CoffeeMachine()
-                //                            .AddWater(30, 90)
-                //                            .AddBeans(new Bean(3, Bean.BeanTypes.Robusta))
-                //                            .GrindBeans()
-                //                            .AddFoamMilk(30);
+                IBeverage Macchiato = new CoffeeMachine()
+                    .AddWater(30, 90)
+                    .AddBeans(new Bean(3, Bean.BeanTypes.Robusta))
+                    .GrindBeans()
+                    .AddFoamMilk(30)
+                    .ToBeverage();
 
-                //CoffeeMachine mocha = new CoffeeMachine()
-                //    .AddWater(30, 90)
-                //    .AddBeans(new Bean(3, Bean.BeanTypes.Excelsa))
-                //    .GrindBeans()
-                //    .AddChocolateSyrup(10)
-                //    .AddMilk(30);
+                IBeverage Mocha = new CoffeeMachine()
+                    .AddWater(30, 90)
+                    .AddBeans(new Bean(3, Bean.BeanTypes.Excelsa))
+                    .GrindBeans()
+                    .AddChocolateSyrup(10)
+                    .AddMilk(30)
+                    .ToBeverage();
 
-                IBeverage felixSpeziale = new CoffeeMachine()
+
+                IBeverage FelixSpeziale = new CoffeeMachine()
                  .AddWater(30, 85)
                  .AddBeans(new Bean(5, Bean.BeanTypes.Arabica))
                  .GrindBeans()
@@ -51,7 +65,7 @@ namespace BaristaApi
                 //  CoffeeMachine brew2 = new CoffeeMachine().AddWater(25, 85).AddMilk(20).AddChocolateSyrup(10).AddBeans(new Bean(4, Bean.BeanTypes.Arabica)).GrindBeans();
 
                 Console.WriteLine("\nBrew is done!");
-                Console.WriteLine("\nEnjoy your " + felixSpeziale.ToString() + " :)");
+                Console.WriteLine("\nEnjoy your " + FelixSpeziale.ToString() + " :)");
                 //Console.WriteLine("\nEnjoy your " + brew2.ToBeverage() + " :)");
                 // Console.WriteLine("\nEnjoy your " + espresso.ToBeverage() + " :)");
             }
