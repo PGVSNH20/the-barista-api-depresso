4fdf583db9feef97044a0ad0cca3111412c07d65
Marcus Broman <mbroman23@gmail.com>
2021-03-04 12:24:05 +0100

    Added test for latte and minor changes

--- BaristaApi.Test/FluentApiTests.cs
+++ BaristaApi.Test/FluentApiTests.cs
@@ -6,42 +6,49 @@ namespace BaristaApi.Tests
     public class FluentApiTests
     {
         [Fact]
-        public void When_AddingWaterAndBeans_Expect_Espresso()
+        public void Expect_Espresso()
         {
             var beverage = new CoffeeMachine().AddWater(50, 90).AddBeans(new Bean(10, Bean.BeanTypes.Robusta)).GrindBeans().ToBeverage();
             Assert.IsType<Espresso>(beverage);
         }
 
         [Fact]
-        public void When_AddingWaterAndBeans_Expect_Cappuccino()
+        public void Expect_Cappuccino()
         {
             var beverage = new CoffeeMachine().AddWater(50, 85).AddBeans(new Bean(5, Bean.BeanTypes.Arabica)).GrindBeans().AddMilk(25).AddFoamMilk(10).ToBeverage();
             Assert.IsType<Cappuccino>(beverage);
         }
 
         [Fact]
-        public void When_AddingWaterAndBeans_Expect_Americano()
+        public void Expect_Americano()
         {
             var beverage = new CoffeeMachine().AddWater(50, 85).AddBeans(new Bean(5, Bean.BeanTypes.Arabica)).GrindBeans().AddWater(20, true).ToBeverage();
             Assert.IsType<Americano>(beverage);
         }
 
         [Fact]
-        public void When_AddingWaterAndBeans_Expect_Macchiato()
+        public void Expect_Macchiato()
         {
             var beverage = new CoffeeMachine().AddWater(50, 90).AddBeans(new Bean(3, Bean.BeanTypes.Robusta)).GrindBeans().AddFoamMilk(30).ToBeverage();
             Assert.IsType<Macchiato>(beverage);
         }
 
         [Fact]
-        public void When_AddingWaterAndBeans_Expect_Mocha()
+        public void Expect_Mocha()
         {
             var beverage = new CoffeeMachine().AddWater(30, 90).AddBeans(new Bean(3, Bean.BeanTypes.Excelsa)).GrindBeans().AddChocolateSyrup(10).AddMilk(30).ToBeverage();
             Assert.IsType<Mocha>(beverage);
         }
 
         [Fact]
-        public void When_AddingWaterAndBeans_Expect_FelixSpeziale()
+        public void Expect_Latte()
+        {
+            var beverage = new CoffeeMachine().AddWater(50, 90).AddBeans(new Bean(10, Bean.BeanTypes.Robusta)).GrindBeans().AddMilk(35).ToBeverage();
+            Assert.IsType<Latte>(beverage);
+        }
+
+        [Fact]
+        public void Expect_FelixSpeziale()
         {
             var beverage = new CoffeeMachine().AddWater(30, 85).AddBeans(new Bean(5, Bean.BeanTypes.Arabica)).GrindBeans().AddAlcohol(50).AddMilk(20).ToBeverage();
             Assert.IsType<FelixSpeziale>(beverage);
